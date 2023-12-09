1ac9ad74cc195484198a690e37dbf7f7547aef80
vi369 <vikashverma3001@gmai.com>
2023-12-09 14:01:25 +0530

    resolve

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const connectToDb = require('./config/db.js')
 const {home} = require('./controllers/userControler.js')
+const cors = require('cros');
 const app = express();
 
 connectToDb();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:this.true}))
+app.use(cors());
 
 const userRoutes = require('./routes/userRoutes.js');
 app.use('/', userRoutes)
